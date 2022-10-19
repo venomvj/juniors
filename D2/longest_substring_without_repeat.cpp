a7c6a4c72a3c93921cddd315d32d77ac2fa16a1c
venomvj <vijaylowada2002@gmail.com>
2022-10-19 20:16:38 +0530

    sol2

--- D2/longest_substring_without_repeat.cpp
+++ D2/longest_substring_without_repeat.cpp
@@ -1,24 +1,27 @@
-// CPP program to demonstrate the
-// set::find() function
 #include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
-int main()
+
+void solution(){
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	int x=0;
+	for(int i = 0; i< s.length()-1; i++){
+		if(s[i]==s[i+1])
+			++x;
+	}
+	cout<<x<<"\n";
+}
+
+
+int32_t main()
 {
-
-	// Initialize set
-	set<int> s;
-
-	s.insert(1);
-	s.insert(4);
-	s.insert(2);
-	s.insert(5);
-	s.insert(3);
-
-	// iterator pointing to
-	// position where 3 is
-	auto pos = s.find(5);
-
-	// prints the set elements
-	cout<< *pos<<endl;
-	return 0;
+	ll t;
+	cin>>t;
+	while(t--){
+		solution();
+	}
+return 0;
 }
